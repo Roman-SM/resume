c030f885cb567c086632009287a6dbddc6c51600
Рома <user@MacBook-Pro-user.local>
2023-05-28 07:02:56 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,34 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяц',
+  address: 'Ivano-Frankivsk',
+}
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:ivanov@mail.com',
+    },
+    phone: {
+      text: '380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -19,14 +47,234 @@ router.get('/', function (req, res) {
 // ================================================================
 
 //              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/summary', function (req, res) {
+router.get('/skills', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('summary', {
+  res.render('skills', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      hobbies: [
+        {
+          name: 'Terminal',
+          isMain: true,
+        },
+        {
+          name: 'Insomnia',
+          isMain: true,
+        },
+        {
+          name: 'Slak',
+          isMain: false,
+        },
+      ],
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 5,
+        },
+        {
+          name: 'Git',
+          point: 10,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/summary', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('Summary', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of 
+      experience in development. Whenever I start to 
+      work on a new project I learn the domain and try 
+      to understand the idea of the project. Good team 
+      player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from 
+      different platforms odds and sport statistics 
+      tournament position, goals etc, analyzing by 
+      simple mathematics models and preparing 
+      probability for such events like: money line - 
+      first win / draw / second win, totals etc`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    //                  ↑↑ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      certificates: [
+        {
+          name: 'Сертифікат про закінчення професійного ліцею',
+          id: 1297,
+        },
+        {
+          name: 'Сетрифікат про закінчення професійно-художнього училища',
+          id: 4212,
+        },
+        {
+          name: 'Сетрифікат про закінчення професійно-технічного училища',
+          id: 4673,
+        },
+      ],
+      education: [
+        {
+          name: 'професійно-технічне училище відповідного профілю',
+          isEnd: true,
+        },
+        {
+          name: 'художнє професійно-технічне училище',
+          isEnd: true,
+        },
+        {
+          name: 'професійне училище соціальної реабілітації',
+          isEnd: false,
+        },
+        {
+          name: 'професійний ліцей',
+          isEnd: true,
+        },
+        {
+          name: 'професійний ліцей відповідного профілю',
+          isEnd: true,
+        },
+        {
+          name: 'професійно-художнє училище',
+          isEnd: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    //                  ↑↑ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT-Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about:
+                'Airbnb competitor. High-load application for searching apartments',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              avards: [
+                {
+                  name: 'Background verification - is a feature that provides users to prove that they ar real persons.',
+                },
+                {
+                  name: 'Preparing SEO optitmized pages.',
+                },
+              ],
+              stackAmount: 43,
+              awardAmount: 29,
+            },
+          ],
+        },
+      ],
+    },
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
